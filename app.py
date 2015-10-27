297056e950080581a46071675431b09c48bb9766
mirgags <markmiraglia@gmail.com>
2015-10-27 15:55:36 -0700

    refactored parser

--- app.py
+++ app.py
@@ -9,6 +9,31 @@ def parseCsv(csvFile):
             theList.append(row)
     return theList
 
+def totalHours(rowList):
+    sumDict = {}
+    for row in rowList:
+        if rowList.index(row) > 0:
+            try:
+                sumDict[row[1]][row[2]] += float(row[16].replace(',',''))
+            except KeyError:
+                try:
+                    sumDict[row[1]][row[2]] = float(row[16].replace(',',''))
+                except KeyError:
+                    sumDict[row[1]] = { row[2]: float(row[16].replace(',','')) }
+    regexp = re.compile('maintenance|t\/m', re.I)
+    for key in sumDict:
+        total = 0.0
+        for subKey in sumDict[key]:
+            #matchObj = regexp.search(subKey)
+            #if matchObj:
+            total += sumDict[key][subKey]
+                #print key + ' total: ' + str(total)
+        sumDict[key]['Total'] = total
+    for key in sumDict:
+        print key + ': ' + json.dumps(sumDict[key]) + '\n\n'
+    return sumDict
+
+
 def webCalls():
     theJson = { "method": "getApiForUsername",
                 "params": {"username": "nutshelladmin@pint.com"},
@@ -36,25 +61,5 @@ def webCalls():
 
 if __name__ == '__main__':
     rowList = parseCsv('harvest.csv')
-    sumDict = {}
-    for row in rowList:
-        if rowList.index(row) > 0:
-            try:
-                sumDict[row[1]][row[2]] += float(row[16].replace(',',''))
-            except KeyError:
-                try:
-                    sumDict[row[1]][row[2]] = float(row[16].replace(',',''))
-                except KeyError:
-                    sumDict[row[1]] = { row[2]: float(row[16].replace(',','')) }
-    regexp = re.compile('maintenance|t\/m', re.I)
-    for key in sumDict:
-        total = 0.0
-        for subKey in sumDict[key]:
-            #matchObj = regexp.search(subKey)
-            #if matchObj:
-            total += sumDict[key][subKey]
-                #print key + ' total: ' + str(total)
-        sumDict[key]['Total'] = total
-    for key in sumDict:
-        print key + ': ' + json.dumps(sumDict[key]) + '\n\n'
+    totalHours(rowList)
 
